4c62c3e99edd2010720bb3b6742232dc121f877a
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2026-04-10 15:10:05 +0700

    Prakt 1.b

--- Jobsheet5/SortingMain11.java
+++ Jobsheet5/SortingMain11.java
@@ -9,5 +9,15 @@ public class SortingMain11 {
         dataurut1.bubbleSort();
         System.out.println("data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        int b[]= {30, 20, 2, 8, 14};
+
+        Sorting11 dataurut2 = new Sorting11(b, b.length);
+
+        System.out.println("Data awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("data sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }
 }
