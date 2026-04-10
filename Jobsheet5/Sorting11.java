6129e4961e1a7ddde2138ef94021921b67899b65
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2026-04-10 15:32:37 +0700

    praktikum 1.c

--- Jobsheet5/Sorting11.java
+++ Jobsheet5/Sorting11.java
@@ -45,4 +45,16 @@ public class Sorting11 {
         }
     }
 
+    void insertionSort(){
+        for (int i = 1; i <= data.length-1; i++) {
+            int temp=data[i];
+            int j=i-1;
+            while (j>=0 && data[j]>temp) {
+                data[j+1]=data[j];
+                j--;
+            }
+            data[j+1]=temp; 
+        }
+    }
+
 }
--- Jobsheet5/SortingMain11.java
+++ Jobsheet5/SortingMain11.java
@@ -7,7 +7,7 @@ public class SortingMain11 {
         System.out.println("Data awal 1");
         dataurut1.tampil();
         dataurut1.bubbleSort();
-        System.out.println("data sudah diurutkan dengan BUBBLE SORT (ASC)");
+        System.out.println("Data sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
 
         int b[]= {30, 20, 2, 8, 14};
@@ -17,7 +17,17 @@ public class SortingMain11 {
         System.out.println("Data awal 2");
         dataurut2.tampil();
         dataurut2.SelectionSort();
-        System.out.println("data sudah diurutkan dengan SELECTION SORT (ASC)");
+        System.out.println("Data sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        int c[]= {40, 10, 4, 9, 3};
+
+        Sorting11 dataurut3 = new Sorting11(c, c.length);
+
+        System.out.println("Data awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("Data sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }
 }
