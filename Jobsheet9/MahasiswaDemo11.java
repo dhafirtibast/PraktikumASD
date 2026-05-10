f6303132d2a934fcfecb56335dcaaffb56bea4af
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2026-05-10 21:45:48 +0700

    commit

--- Jobsheet9/MahasiswaDemo11.java
+++ Jobsheet9/MahasiswaDemo11.java
@@ -12,6 +12,8 @@ public class MahasiswaDemo11 {
             System.out.println("2. Menilai Tugas");
             System.out.println("3. Melihat Tugas Teratas");
             System.out.println("4. Melihat Daftar Tugas");
+            System.out.println("5. Lihat Tugas Terbawah");
+            System.out.println("6. Jumlah Tugas Dikumpulkan");
             System.out.print("Pilih: ");
             pilih = scan.nextInt();
             scan.nextLine();
