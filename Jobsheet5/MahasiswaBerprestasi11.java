c3f0852cd7d256977fe49fc44a343c46ed9dd32b
Dhafir Tsabit <dhafirtsaabit@gmail.com>
2026-06-06 17:53:46 +0700

    Praktikum 6 Percobaan 1

--- Jobsheet5/MahasiswaBerprestasi11.java
+++ Jobsheet5/MahasiswaBerprestasi11.java
@@ -51,26 +51,58 @@ public class MahasiswaBerprestasi11 {
     }
 
     void insertionSort() {
-    for (int i = 1; i < listMhs.length; i++) {
-        Mahasiswa11 temp = listMhs[i];
-        int j = i;
-        while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
-            listMhs[j] = listMhs[j - 1];
-            j--;
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa11 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk > temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
         }
-        listMhs[j] = temp;
-    }
-}
+    }  
 
     void insertionSortDescending() {
-    for (int i = 1; i < listMhs.length; i++) {
-        Mahasiswa11 temp = listMhs[i];
-        int j = i;
-        while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
-            listMhs[j] = listMhs[j - 1];
-            j--;
+        for (int i = 1; i < listMhs.length; i++) {
+            Mahasiswa11 temp = listMhs[i];
+            int j = i;
+            while (j > 0 && listMhs[j - 1].ipk < temp.ipk) {
+                listMhs[j] = listMhs[j - 1];
+                j--;
+            }
+            listMhs[j] = temp;
         }
-        listMhs[j] = temp;
     }
-}
+
+    int sequentialSearch(double cari) {
+        int posisi = -1;
+        for (int j = 0; j < listMhs.length; j++) {
+            if (listMhs[j].ipk == cari) {
+                posisi = j;
+                break;
+            }
+        }
+        return posisi;
+    }
+
+    void tampilPosisi(double x, int pos) {
+        if (pos!=-1) {
+            System.out.println("data mahasiswa dengan IPK : " +x+ " ditemukan pada indeks "+pos);
+        } else {
+            System.out.println("data " +x+ " tidak ditemukan");
+        }
+    }
+
+    void tampilDataSearch(double x, int pos) {
+        if (pos!=-1) {
+            System.out.println("nim\t : " + listMhs[pos].nim);
+            System.out.println("nama\t : " + listMhs[pos].nama);
+            System.out.println("kelas\t : " + listMhs[pos].kelas);
+            System.out.println("ipk\t : " + x);
+        } else {
+            System.out.println("Data mahasiswa dengan IPK " +x+ " tidak ditemukan");
+        }
+    }
+
+
 }
--- Jobsheet5/MahasiswaDemo11.java
+++ Jobsheet5/MahasiswaDemo11.java
@@ -3,8 +3,10 @@ import java.util.Scanner;
 public class MahasiswaDemo11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        MahasiswaBerprestasi11 list = new MahasiswaBerprestasi11(5);
-        for (int i = 0; i < 5; i++) {
+        int jumMhs=5;
+        MahasiswaBerprestasi11 list = new MahasiswaBerprestasi11(jumMhs);
+
+        for (int i = 0; i < jumMhs; i++) {
             System.out.println("Masukkan Data Mahasiswa ke-" + (i + 1));
             System.out.print("NIM   : ");
             String nim = sc.nextLine();
@@ -15,29 +17,44 @@ public class MahasiswaDemo11 {
             System.out.print("IPK   : ");
             double ipk = sc.nextDouble();
             sc.nextLine();
-
+            System.out.println("---------------------------------------------");
             Mahasiswa11 m = new Mahasiswa11(nim, nama, kelas, ipk);
             list.tambah(m);
         }
 
+       // System.out.println("Data mahasiswa sebelum sorting: ");
+       // list.tampil();
+//
+       // System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
+       // list.bubbleSort();
+       // list.tampil();
+//
+       // System.out.println("Data mahasiswa setelah sorting dengan Selection Sort (ASC):");
+       // list.selectionSort();
+       // list.tampil();
+//
+       // System.out.println("Data mahasiswa setelah sorting dengan Insertion Sort (ASC):");
+       // list.insertionSort();
+       // list.tampil();
+//
+       // System.out.println("Data mahasiswa setelah sorting dengan Insertion Sort (DESC):");
+       // list.insertionSortDescending();
+       // list.tampil();
 
-        System.out.println("Data mahasiswa sebelum sorting: ");
+        //Jobsheet 6
         list.tampil();
 
-        System.out.println("Data mahasiswa setelah sorting berdasarkan IPK (DESC) : ");
-        list.bubbleSort();
-        list.tampil();
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("Pencarian data");
+        System.out.println("--------------------------------------------------------------");
+        System.out.println("masukkan ipk mahasiswa yang dicari : ");
+        System.out.print("IPK: ");
+        double cari = sc.nextDouble();
 
-        System.out.println("Data mahasiswa setelah sorting dengan Selection Sort (ASC):");
-        list.selectionSort();
-        list.tampil();
-
-        System.out.println("Data mahasiswa setelah sorting dengan Insertion Sort (ASC):");
-        list.insertionSort();
-        list.tampil();
-
-        System.out.println("Data mahasiswa setelah sorting dengan Insertion Sort (DESC):");
-        list.insertionSortDescending();
-        list.tampil();
+        System.out.println("menggunakan sequential searching");
+        double posisi = list.sequentialSearch(cari);
+        int pss= (int)posisi;
+        list.tampilPosisi(cari, pss);
+        list.tampilDataSearch(cari, pss);
     }
 }
